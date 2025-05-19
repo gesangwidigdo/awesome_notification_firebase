d0fa81fe07c3e33458064650418589e61961526a
Gesang Widigdo <gesangwidigdo@gmail.com>
2025-05-19 18:02:46 +0700

    feat: integrate awesome notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,35 @@
 import 'package:awesome_notification_firebase/firebase_options.dart';
+import 'package:awesome_notification_firebase/screens/home_screen.dart';
+import 'package:awesome_notification_firebase/screens/login.dart';
+import 'package:awesome_notification_firebase/screens/register.dart';
+import 'package:awesome_notification_firebase/screens/second_screen.dart';
+import 'package:awesome_notification_firebase/services/notification_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await NotificationService.initializeNotification();
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return MaterialApp(
+      initialRoute: 'login',
+      routes: {
+        'home': (context) => const HomeScreen(),
+        'login': (context) => const LoginScreen(),
+        'register': (context) => const RegisterScreen(),
+        'second_screen': (context) => const SecondScreen(),
+      },
+      navigatorKey: navigatorKey,
     );
   }
 }
